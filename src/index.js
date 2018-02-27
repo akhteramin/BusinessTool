b5f9aeb01f6844fef8f870a98fb5576fc6040224
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2018-02-27 23:32:41 +0600

    Basic Redux Setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import Main from './Main';
-import registerServiceWorker from './registerServiceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './styles/index.css';
+// import Main from './Main';
+// import registerServiceWorker from './registerServiceWorker';
+//
+// ReactDOM.render(<Main/>, document.getElementById('root'));
+// registerServiceWorker();
 
-ReactDOM.render(<Main/>, document.getElementById('root'));
-registerServiceWorker();
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import store, { history } from './store'
+import App from './containers/app'
+
+import './styles/index.css'
+
+const target = document.querySelector('#root')
+
+render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <div>
+                <App />
+            </div>
+        </ConnectedRouter>
+    </Provider>,
+    target
+)
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
-import Home from './components/Home';
 import App from './components/App';
 import Login from './components/Login';
 import CreateCampaign from './components/CreateCampaign';
@@ -10,16 +9,15 @@ import NotFound from './components/NotFound';
 export default () => (
     <BrowserRouter>
         <Switch>
-            {/*<Route component={Home}*/}
-                   {/*path="/"*/}
-                   {/*exact />*/}
             <Route component={App}
                    path="/app/home"
                    exact />
             <Route component={Login}
-                   path="/login" />
+                   path="/login"
+                   exact />
             <Route component={CreateCampaign}
-                   path="/app/campaign/new" />
+                   path="/app/campaign/new"
+                   exact />
             <Route component={NotFound} />
         </Switch>
     </BrowserRouter>
