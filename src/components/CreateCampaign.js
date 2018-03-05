1750e399b69b185b52dee17e16d3d0d3007a375a
Tahmid Tanzim <tahmid.tanzim@ipay.com.bd>
2018-03-05 19:00:02 +0600

    IPAY-3345 Campaign Form Submit

--- src/components/CreateCampaign.js
+++ src/components/CreateCampaign.js
@@ -7,63 +7,82 @@ class CreateCampaign extends Component {
     constructor(props) {
         super(props);
         this.backHome = this.backHome.bind(this);
-        this.handleCampaign = this.handleCampaign.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+        this.state = {
+            offerType: '',
+            offerName: ''
+        };
     }
 
     backHome = () => {
         this.props.history.push('/app/home');
     };
-    handleCampaign = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.target);
-        Http.GET('get_posts')
-        .then(
-            ({data: data}) => {
-                console.log(data);
-            },
-            error => {
-                console.log('Error in address update, error: ', error);
-            }
-        );
 
+    onChange = (e) => {
+        // Because we named the inputs to match their corresponding values in state, it's
+        // super easy to update the state
+        const state = this.state;
+        state[e.target.name] = e.target.value;
+        this.setState(state);
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const { offerType, offerName } = this.state;
+
+        // const data = new FormData(e.target);
+        // const data = e.target.value;
+
+        console.log('Post Campaign: ', JSON.stringify({ offerType, offerName }, null, 2));
+
+        // Http.POST('offer', { offerType, offerName })
+        // .then(
+        //     ({data: data}) => {
+        //         console.log(data);
+        //     },
+        //     error => {
+        //         console.log('Error in address update, error: ', error);
+        //     }
+        // );
     };
 
     render() {
+        const {offerType, offerName} = this.state;
         return (
             <div className="row">
                 <Header/>
-                <div className="col-md-12">
-                    <div className="col-md-6 col-md-offset-3">
-                        <Subtitle value="New Campaign"/>
-                        <form onSubmit={ this.handleCampaign }>
-                            <div className="form-group">
-                                <label>Campaign Name</label>
-                                <input type="email" className="form-control" name="campaignName"
-                                       id="exampleInputEmail1" placeholder="Email"/>
-                            </div>
-                            <div className="form-group">
-                                <label>Service List</label>
-                                <input type="password" className="form-control" name="serviceList"
-                                       id="exampleInputPassword1" placeholder="Password"/>
-                            </div>
-                            <div className="form-group">
-                                <label>Campaign File</label>
-                                <div class="fileUpload">
-                                    <input id="uploadBtn3" type="file" class="btn btn-default"/>
-                                </div>
-                                <p className="help-block">csv/excel accepted only</p>
-                            </div>
+                <div className="col-md-4 col-md-offset-4">
+                    <Subtitle value="New Campaign"/>
+                    <form onSubmit={ this.handleSubmit }>
+                        <div className="form-group">
+                            <label>Offer Type</label>
+                            <input type="text"
+                                   className="form-control"
+                                   name="offerType"
+                                   id="offerType"
+                                   placeholder="Offer Type"
+                                   value={ offerType }
+                                   onChange={ this.onChange }/>
+                        </div>
+                        <div className="form-group">
+                            <label>Offer Name</label>
+                            <input type="text"
+                                   className="form-control"
+                                   name="offerName"
+                                   id="offerName"
+                                   placeholder="Offer Name"
+                                   value={ offerName }
+                                   onChange={ this.onChange }/>
+                        </div>
 
-                            <div className="text-center">
-                                <button type="submit" className="btn btn-default">Create Campaign
-                                </button>
-                                <button onClick={ this.backHome }
-                                        className="btn btn-danger margin-left-5">Cancel
-                                </button>
+                        <button type="submit" className="btn btn-default">Create Campaign
+                        </button>
+                        <button onClick={ this.backHome }
+                                className="btn btn-danger margin-left-5">Cancel
+                        </button>
 
-                            </div>
-                        </form>
-                    </div>
+                    </form>
                 </div>
             </div>
         );
