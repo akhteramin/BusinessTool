05305f828fbdf55d088f265da46c5d666ed9202c
Tahmid Tanzim <tahmid.tanzim@ipay.com.bd>
2018-04-23 10:32:31 +0600

    Target Group Created Successfully

--- src/components/TargetGroup.js
+++ src/components/TargetGroup.js
@@ -7,14 +7,24 @@ class TargetGroup extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            criteria: {},
-            logicalCriteriaList: [], // Selected Criteria's id
+            name: '',
+            description: '',
             isActive: true,
-            targetReach: 0
+            logicalCriteriaList: [], // Selected Criteria's id
+            criteria: {},
+            targetReach: 0,
+            error: {
+                name: false,
+                description: false
+            }
         };
 
         this.toggleCriteria = this.toggleCriteria.bind(this);
         this.updateCriteriaReach = this.updateCriteriaReach.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleValidation = this.handleValidation.bind(this);
+        this.isValidForm = this.isValidForm.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     toggleCriteria(name, id) {
@@ -42,7 +52,7 @@ class TargetGroup extends Component {
     updateCriteriaReach() {
         const {logicalCriteriaList: list} = this.state;
 
-        if(list.length) {
+        if (list.length) {
             Http.GET('targetGroups', `/${list.join(',')}/reach`)
             .then(({data}) => {
                 console.log('Reach Count: ', data.count);
@@ -52,6 +62,43 @@ class TargetGroup extends Component {
             })
             .catch(error => console.error(error));
         }
+    }
+
+    handleChange(event) {
+        const {target} = event;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const {name} = target;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleValidation(event) {
+        const {name} = event.target;
+        this.setState(previous_state => ({
+            error: {
+                ...previous_state.error,
+                [name]: !this.state[name]
+            }
+        }));
+    }
+
+    isValidForm() {
+        const {name, description, logicalCriteriaList} = this.state;
+        return name && description && logicalCriteriaList.length;
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const {name, description, logicalCriteriaList, isActive} = this.state;
+        // console.log('Submit: ', JSON.stringify({name, description, logicalCriteriaList, isActive}, null, 2));
+
+        Http.POST('targetGroups', {name, description, logicalCriteriaList, isActive, id: null})
+        .then(response => {
+            console.log('Success create TG: ', JSON.stringify(response, null, 2));
+        })
+        .catch(error => console.error(error));
     }
 
     componentDidMount() {
@@ -73,7 +120,7 @@ class TargetGroup extends Component {
     }
 
     render() {
-        const {criteria, targetReach} = this.state;
+        const {name, description, isActive, criteria, targetReach, error} = this.state;
         return (
             <div>
                 <Title value="Target Group"/>
@@ -81,16 +128,26 @@ class TargetGroup extends Component {
                     <div className="col-md-4">
                         { Object.keys(criteria).length === 0 && criteria.constructor === Object
                             ? <img src={ spinner } style={ {width: 20, height: 20} } alt="Ajax Loader"/>
-                            : <form>
-                                <div className="form-group">
-                                    <label htmlFor="name">TG Name</label>
+                            : <form name="AddTargetGroup" onSubmit={ this.handleSubmit }>
+                                <div className={ 'form-group'.concat(error.name ? ' has-error' : '') }>
+                                    <label className="control-label" htmlFor="name">TG Name</label>
                                     <input type="text"
                                            className="form-control"
                                            id="name"
-                                           placeholder="TG Name"/>
+                                           name="name"
+                                           value={ name }
+                                           onChange={ this.handleChange }
+                                           onBlur={ this.handleValidation }
+                                           placeholder="i.e. Verified Personal Users"
+                                           aria-describedby="helpTGName"/>
+                                    { error.name ? <span id="helpTGName"
+                                                         className="help-block"
+                                                         style={ {marginTop: 0} }>
+                                <small style={ {fontSize: '70%'} }>TG Name Required</small>
+                            </span> : null }
                                 </div>
                                 <div className="form-group">
-                                    <label htmlFor="criteria">TG Criteria</label>
+                                    <label className="control-label" htmlFor="criteria">TG Criteria</label>
                                     <span className="badge pull-right">{ targetReach }</span>
                                     <div className="panel panel-default">
                                         <div className="panel-body">
@@ -117,25 +174,38 @@ class TargetGroup extends Component {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="form-group">
-                                    <label htmlFor="description">Description</label>
+                                <div className={ 'form-group'.concat(error.description ? ' has-error' : '') }>
+                                    <label className="control-label" htmlFor="description">Description</label>
                                     <textarea className="form-control"
                                               id="description"
                                               name="description"
-                                              rows="4"/>
+                                              rows="3"
+                                              value={ description }
+                                              onChange={ this.handleChange }
+                                              onBlur={ this.handleValidation }
+                                              aria-describedby="helpDescription"/>
+                                    { error.description ? <span id="helpDescription"
+                                                         className="help-block"
+                                                         style={ {marginTop: 0} }>
+                                <small style={ {fontSize: '70%'} }>Description Required</small>
+                            </span> : null }
                                 </div>
                                 <div className="form-group">
                                     <div className="checkbox">
                                         <label htmlFor="isActive">
                                             <input type="checkbox"
                                                    id="isActive"
-                                                   name="isActive"/>
-                                            <small style={ {marginTop: 2, display: 'block'} }> Active
-                                            </small>
+                                                   name="isActive"
+                                                   checked={ isActive }
+                                                   onChange={ this.handleChange }/>
+                                            <small style={ {marginTop: 2, display: 'block'} }>Active</small>
                                         </label>
                                     </div>
                                 </div>
-                                <button type="submit" className="btn btn-default">Submit</button>
+                                <button type="submit"
+                                        className="btn btn-default"
+                                        disabled={ !this.isValidForm() }>Submit
+                                </button>
                             </form> }
                     </div>
                     <div className="col-md-7 col-md-offset-1">
